aa282b1df952338d50fd2d64ed36e6c3049ffcd9
dimasbayuseno <dbswhnp@gmail.com>
2023-03-12 11:30:37 +0700

    initial commit

---
+++ banking/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	// define routes
+	http.HandleFunc("/greet", greet)
+
+	// starting server
+	http.ListenAndServe("localhost:8000", nil)
+}
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world")
+}
