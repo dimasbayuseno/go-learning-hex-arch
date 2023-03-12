1c64d699d1b3f3f094ef59c1848e7848d8a072c4
dimasbayuseno <dbswhnp@gmail.com>
2023-03-12 11:43:58 +0700

    learn json encoding

--- banking/main.go
+++ banking/main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Costumer struct {
+	Name    string
+	City    string
+	Zipcode string
+}
+
 func main() {
 
 	// define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/getAllCostumers", getAllCostumer)
 
 	// starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world")
+}
+
+func getAllCostumer(w http.ResponseWriter, r *http.Request) {
+	costumers := []Costumer{
+		{Name: "Dimas", City: "Jogja", Zipcode: "12345"},
+		{Name: "Soultan", City: "Jogja", Zipcode: "12345"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(costumers)
 }
